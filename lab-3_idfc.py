2b8b4204c35593c295e1ea7ad3240cca99be6599
S.K. Balaji <60575418+BALAJI-SK@users.noreply.github.com>
2021-12-02 12:08:23 +0530

    IDDFC Method

--- lab-3_idfc.py
+++ lab-3_idfc.py
@@ -75,8 +75,7 @@ def iddfs(src, target, depth):
     return -1
 
 
-# src = [1,2,3,-1,4,5,6,7,8]
-# target=[1,2,3,6,4,5,-1,7,8]
+
 src = [1, 2, 3, -1, 4, 5, 6, 7, 8]
 target = [1, 2, 3, 4, 5, -1, 6, 7, 8]
 depth = 2
